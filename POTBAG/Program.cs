189741692190a12808e32e5d5797d827d704910c
Benno4president <kbengt19@student.aau.dk>
2021-04-08 12:12:59 +0200

    added main + filehandler

--- POTBAG/Program.cs
+++ POTBAG/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using POTBAG.CSTtoAST;
+using System;
 
 namespace POTBAG
 {
@@ -6,7 +9,35 @@ namespace POTBAG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string stream = FileHandler.readFromInputStream("UpdatedPseudoDrageTestFragment.txt");
+
+            ICharStream input = CharStreams.fromString(stream);
+            ITokenSource lexer = new BetterAdvGmLexer(input);
+            ITokenStream tokenStream = new CommonTokenStream(lexer);
+            BetterAdvGmParser parser = new BetterAdvGmParser(tokenStream);
+
+            //set start node
+            var cst = parser.prog(); ;
+
+            //var ast = new BetterAdvGmASTVisitor().visitProg(cst);
+
+
+            //var tree = Trees.ToStringTree(cst,parser);
+            //Console.WriteLine(tree);
+
+            //System.out.println(ast);
+            //ParseTreeWalker walker = new ParseTreeWalker();
+            //walker.walk(visitor, parseTree);
+
+            //System.out.println(parseTree.toStringTree());
+            //System.out.println(visitor.numbers.get(parseTree));
+
+
+
+            //lexer - parser - visitor - walker
+            //run.LexParsWalk(Stream);
+            //fileWriter.WriteToFile();
+            //fileWriter.PrintCCodeDebug();
         }
     }
-}
+    }
