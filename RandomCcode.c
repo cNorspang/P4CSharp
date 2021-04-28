fb63e7aae7cd64e120356aa5b648f0edc9056ef8
Benno4president <krijoh.b@gmail.com>
2021-04-28 14:54:37 +0200

    i just did c some random code

---
+++ RandomCcode.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void Init(){
+    time_t t;
+   /* Intializes random number generator */
+   srand((unsigned) time(&t));
+}
+
+int RndNum(int to, int from){
+    return (rand() % (from + 1 - to)) + to;
+}
+
+int main () {
+   int i;
+
+   Init();
+   /* Print 5 random numbers from 0 to 49 */
+   for( i = 0 ; i < 10 ; i++ ) {
+      printf("%d\n", RndNum(10, 20));
+    }
+   
+   return(0);
+}
+/*
+srand((unsigned)time(NULL));
+
+int RndNum(int to, int from){
+    int rNum = rand();
+    return (rNum % (from - to)) + to;
+}
+
+int main(){
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d. %d", i, RndNum(10, 20));
+    }
+    return 0;
+}
+*/
