e14d4174f2c7262ae3895c3f0c2914cab8394839
Benno4president <krijoh.b@gmail.com>
2021-05-03 14:30:25 +0200

    idk

--- test.c
+++ test.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printText(char* types[256], char* toPrint[256]){
+    printf(types, toPrint);
+}
+
+void printTextWithChoice(){
+    
+}
+
 struct GENERATED_PLAYER_STRUCT { 
     int x;
 };
 struct GENERATED_PLAYER_STRUCT xCarier;
 
 void locationOne(){
-    printf("%s","\nlocation One");
+    printText("%s","\nlocation One");
     getchar();
 }
+
+
 
 int main(void){
     xCarier.x = 10;
