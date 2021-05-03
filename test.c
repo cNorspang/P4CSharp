2969426b587cf517c4b66f39c572a1b0921131ad
Benno4president <krijoh.b@gmail.com>
2021-05-03 20:23:22 +0200

    pushing is a religion now

--- test.c
+++ test.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printText(char types[256], char toPrint[256]){
+    printf(types, toPrint);
+
+}
+
+void printTextWithChoice(){
+    
+}
+
 struct GENERATED_PLAYER_STRUCT { 
     int x;
 };
 struct GENERATED_PLAYER_STRUCT xCarier;
 
 void locationOne(){
-    printf("%s","\nlocation One");
+    printText("%s","\nlocation One");
     getchar();
 }
-
-
-
 
 
 
@@ -21,5 +27,4 @@ int main(void){
     printf("%d", xCarier.x);
     locationOne();
     return 0;
-    
 }
