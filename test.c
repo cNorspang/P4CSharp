156a76319079f390de8c9ef4bbf819fa8c518b5d
Benno4president <krijoh.b@gmail.com>
2021-05-03 13:55:14 +0200

    idk

---
+++ test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct GENERATED_PLAYER_STRUCT { 
+    int x;
+};
+struct GENERATED_PLAYER_STRUCT xCarier;
+
+void locationOne(){
+    printf("%s","\nlocation One");
+    getchar();
+}
+
+
+
+
+
+
+int main(void){
+    xCarier.x = 10;
+    printf("%d", xCarier.x);
+    locationOne();
+    return 0;
+    
+}
